dad6c33d8b75005608f35bafebdbb2b4a2726cd4
AllanRaf <rafale.uk@googlemail.com>
2019-08-29 21:41:32 +0200

    Set Up Form Validation

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,12 +1,36 @@
 function submitComment(){
 
+
     const inputField = document.getElementById("name");
 
-    const name = inputField.value;
+    let name = inputField.value;
 
     const textField = document.getElementById('msg');
     const msg = textField.value;
-    //console.log(msg);
+
+    //exit if data is not valid
+    if(doesNotPassAllValidations(name, msg)){
+
+        return null;
+
+    }
+
+    //capitalise first letter of name if necessary
+
+    //if(Boolean(name.match(/^[a-z]/))) also works
+
+    let regEx = /^[a-z]/;
+
+    console.log(regEx.test(name));
+    
+    if(regEx.test(name)){
+
+        name = name.charAt(0).toUpperCase() + name.slice(1);
+
+    }
+
+    //data valid, carry on
+    
     const comment = document.createElement("section");
 
     const h3 = document.createElement("h3");
@@ -21,18 +45,46 @@ function submitComment(){
     comment.appendChild(h3);
     comment.appendChild(p);
 
-    //console.log(comment);
 
     const commentSection = document.getElementById("comments");
-    console.log(commentSection);
     commentSection.appendChild(comment);
+
 
     //reset
     inputField.value = null;
     textField.value = null;
 
+}
+
+function doesNotPassAllValidations(name, msg){
 
 
+    if(!name){
+
+        alert("Name and/or Message must not be left blank!");
+        return true;
+
+    }else if(!msg){
+
+        alert("Message must not be left blank!");
+        return true;
+
+    }else if(msg.length>250){
+
+        alert("Message must be under 250 characters in length!");
+        return true;
+    
+    }
+
+    //test comments for inappropriate comments
+
+    regEx = /fuck|shit|bastard|bitch|cock/ig;
+
+    if(regEx.test(msg)){
+
+        alert("Warning: this comment has been flagged as offensive...");
+        alert("But we'll post it anyway!");
+    }
 
 
 }

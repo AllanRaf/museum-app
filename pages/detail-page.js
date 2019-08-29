4d895b221e95c1a162d22e4ef257de7c6297ca88
AllanRaf <rafale.uk@googlemail.com>
2019-08-29 17:24:06 +0200

    added functions to create elements on form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,5 +1,30 @@
 function submitComment(){
 
-    console.log("Hello!");
+    const inputField = document.getElementById("name");
+
+    const name = inputField.value;
+
+    const textField = document.getElementById('msg');
+    const msg = textField.value;
+    //console.log(msg);
+    const comment = document.createElement("section");
+
+    const h3 = document.createElement("h3");
+
+    const p = document.createElement("p");
     
+    h3.innerHTML = `${name} said:`;
+
+    p.innerHTML = msg;
+
+    comment.classList.add("comment");
+    comment.appendChild(h3);
+    comment.appendChild(p);
+
+    console.log(comment);
+
+
+
+
+
 }
